049f568f1ebfbfdf6a6f385b9f8ad2c1e9adefda
Benjamin Powers <benawabe896@gmail.com>
2012-11-27 06:16:54 +0000

    solve

--- Problem_51.py
+++ Problem_51.py
@@ -26,14 +26,48 @@ def getPrimes(n):
 		m=2*i+3
 	return [2]+[x for x in s if x]
 
-primes = getPrimes(100)
+primes = getPrimes(1000000)
 
-categories = {0:{},1:{},2:{},3:{},4:{},5:{}}
-
+categories = {1:{},2:{},3:{},4:{},5:{},6:{},7:{}}
 for p in primes:
+	if len(str(p)) < 5:
+		continue
 	# iterate through the characters of the primes and assign them to 
 	# the categories appropriately i.e.
 	# 01 and 10 are the categories of two digits
 	# 001 010 100 011 101 110 and the categories for three digits
+	for x in xrange(1,int(str(int("1"*len(str(p)))),2)):
+		mask = str(bin(x)[2:]).zfill(20)
+		newSt = list(str(p))
 
+		prevX = ''
+		for y in xrange(0,len(str(p))):
+			incr = -1*(y+1)
+			if mask[incr] == '1':
+				if prevX == '':
+					prevX = newSt[incr]
+				if prevX != newSt[incr]:
+					continue
+				newSt[incr] = 'x'
+		newSt = ''.join(newSt)
+		lsp = len(str(p))
+		currCat = categories[lsp]
+		if newSt not in categories[lsp]:
+			categories[lsp][newSt] = [] 
+		if p not in  categories[lsp][newSt]:
+			categories[lsp][newSt].append(p)
+
+maxLen = 0
+maxVal = ''
+maxVals = ''
+for lenn in categories:
+	for newSt in sorted(categories[lenn]):
+		vals = categories[lenn][newSt]
+		if len(vals) == 8:
+			maxVal = newSt
+			maxLen = len(vals)
+			maxVals = vals
+			break
+	
+print maxVal, maxLen, maxVals
 
